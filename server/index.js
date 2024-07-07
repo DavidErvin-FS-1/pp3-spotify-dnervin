6dc2ac80729c4ca33e1b45f40e03cade17f1350f
David Ervin <dnervin@student.fullsail.edu>
2024-07-07 11:01:44 -0400

    Add server setup and basic route

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+
+// Require dotenv
+require('dotenv').config()
+
+// Initialize the app
+const app = express()
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Spotify API server is running.. API still in development')
+})
+
+// Routes
+
+// Start the server
+const port = process.env.PORT || 3001
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
