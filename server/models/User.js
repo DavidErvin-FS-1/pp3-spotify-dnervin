0e93623928f68a4af6b72f1f7300b460000889a2
David Ervin <dnervin@student.fullsail.edu>
2024-07-14 00:32:49 -0400

    Add password hashing and comparison methods to User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 // Create a new schema for the user
 const userSchema = new mongoose.Schema(
@@ -21,3 +22,19 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 )
+
+// Hash the password before saving the user
+userSchema.pre('save', async function (next) {
+  const user = this
+
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 10)
+  }
+
+  next()
+})
+
+// Compare the password with the hashed password
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password)
+}
