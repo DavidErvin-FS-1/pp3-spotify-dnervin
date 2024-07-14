21f58859b2bdd43b49ad488082eedb5bffaf436b
David Ervin <dnervin@student.fullsail.edu>
2024-07-14 01:09:14 -0400

    Update User model to comment out unused code

--- server/models/User.js
+++ server/models/User.js
@@ -1,21 +1,21 @@
 const mongoose = require('mongoose')
-const bcrypt = require('bcryptjs')
+// const bcrypt = require('bcryptjs')
 
-// Create a new schema for the user
+//? Create a new schema for the user
 const userSchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
+    // name: {
+    //   type: String,
+    //   required: true,
+    // },
+    // email: {
+    //   type: String,
+    //   unique: true,
+    // },
+    // password: {
+    //   type: String,
+    //   required: true,
+    // },
     jwtToken: {
       type: String,
     },
@@ -23,20 +23,20 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 )
 
-// Hash the password before saving the user
-userSchema.pre('save', async function (next) {
-  const user = this
+//? Hash the password before saving the user
+// userSchema.pre('save', async function (next) {
+//   const user = this
 
-  if (user.isModified('password')) {
-    user.password = await bcrypt.hash(user.password, 10)
-  }
+//   if (user.isModified('password')) {
+//     user.password = await bcrypt.hash(user.password, 10)
+//   }
 
-  next()
-})
+//   next()
+// })
 
-// Compare the password with the hashed password
-userSchema.methods.comparePassword = async function (password) {
-  return await bcrypt.compare(password, this.password)
-}
+//? Compare the password with the hashed password
+// userSchema.methods.comparePassword = async function (password) {
+//   return await bcrypt.compare(password, this.password)
+// }
 
 module.exports = mongoose.model('User', userSchema)
