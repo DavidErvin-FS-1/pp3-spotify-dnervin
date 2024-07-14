40245534795687cc5da865a6a243e2a73b778ed0
David Ervin <dnervin@student.fullsail.edu>
2024-07-14 01:11:05 -0400

    Refactor comments for better readability and consistency

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,13 +2,13 @@ const router = require('express').Router()
 
 const authCtrl = require('../controllers/authCtrl')
 
-// Login route to authorize the user with Spotify API
+//? Login route to authorize the user with Spotify API
 router.get('/login', authCtrl.login)
 
-// Callback route to get the access token
+//? Callback route to get the access token
 router.get('/callback', authCtrl.callback)
 
-// Refresh route to get the new access token
+//? Refresh route to get the new access token
 router.get('/refresh', authCtrl.refresh)
 
 module.exports = router
