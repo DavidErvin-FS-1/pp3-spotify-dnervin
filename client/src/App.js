4fcfcd2d8c350f5ac90068487354b956a09c23fb
David Ervin <dnervin@student.fullsail.edu>
2024-07-27 23:17:39 -0400

    chore: Update App.js to handle Spotify authentication and redirect
    This commit updates the `App.js` file to handle Spotify authentication and redirect. It adds logic to check for a token in the URL parameters and store it in the local storage. If a token is found, it sets the token and its expiration time in the local storage and redirects the user to the home page. If no token is found, it displays a sign-in message and a link to the Spotify authentication page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { FaSpotify } from 'react-icons/fa6'
+import SpotifyApp from './pages/SpotifyApp'
+import './styles/App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const urlParams = new URLSearchParams(window.location.search)
+  const token = urlParams.get('token')
+
+  if (token) {
+    localStorage.setItem('token', token)
+    localStorage.setItem(
+      'tokenExpiration',
+      new Date().getTime() + 3600 * 1000
+    ) // Set expiration to 1 hour from now
+    window.location.href = '/'
+  }
+
+  const storedToken = localStorage.getItem('token')
+
+  if (!storedToken) {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <FaSpotify className='App-logo' />
+          <h1>Please Sign In Using Spotify</h1>
+          <p>
+            In order to use this app, you need to sign in using your
+            Spotify account.
+          </p>
+          <a
+            className='App-link'
+            href='http://localhost:3001/auth/login'
+            rel='noopener noreferrer'
+          >
+            Sign In
+          </a>
+        </header>
+      </div>
+    )
+  } else {
+    return <SpotifyApp storedToken={storedToken} />
+  }
 }
 
-export default App;
+export default App
