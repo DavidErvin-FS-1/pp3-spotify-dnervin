c3de5c988e25fdac9ab602f6a2bfabf8d0374807
David Ervin <dnervin@student.fullsail.edu>
2024-07-27 22:55:59 -0400

    chore: Update auth.js to handle Spotify authorization and token retrieval
    Handles '/login', '/callback' & '/refresh'

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,14 +1,130 @@
-const router = require('express').Router()
+const axios = require('axios')
+const express = require('express')
+const genRandomString = require('../utils/genRandomString')
+const jwt = require('jwt-simple')
+const SpotifyToken = require('../models/SpotifyToken')
+const validateJWT = require('../middlewares/validateJWT')
+require('dotenv').config()
 
-const authCtrl = require('../controllers/authCtrl')
+const router = express.Router()
 
-//? Login route to authorize the user with Spotify API
-router.get('/login', authCtrl.login)
+const {
+  SPOTIFY_CLIENT_ID,
+  SPOTIFY_CLIENT_SECRET,
+  SPOTIFY_REDIRECT_URI,
+  JWT_SECRET,
+} = process.env
 
-//? Callback route to get the access token
-router.get('/callback', authCtrl.callback)
+// Route to get the Spotify authorization URL
+router.get('/login', (req, res) => {
+  const scope = 'user-read-private user-read-email'
+  const state = genRandomString(16)
 
-//? Refresh route to get the new access token
-router.get('/refresh', authCtrl.refresh)
+  res.redirect(
+    `https://accounts.spotify.com/authorize?response_type=code&client_id=${SPOTIFY_CLIENT_ID}&scope=${scope}&redirect_uri=${SPOTIFY_REDIRECT_URI}&state=${state}`
+  )
+})
+
+// Route to get the Spotify access token
+router.get('/callback', async (req, res) => {
+  const { code } = req.query
+
+  if (!code) {
+    return res.send('Invalid request')
+  }
+
+  try {
+    const response = await axios({
+      method: 'post',
+      url: 'https://accounts.spotify.com/api/token',
+      params: {
+        grant_type: 'authorization_code',
+        code,
+        redirect_uri: SPOTIFY_REDIRECT_URI,
+      },
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic ${Buffer.from(
+          `${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`
+        ).toString('base64')}`,
+      },
+    })
+
+    const { access_token, token_type, expires_in, refresh_token } =
+      response.data
+
+    // Check if the token already exists
+    const existingToken = await SpotifyToken.findOne({
+      refresh_token,
+    })
+
+    if (!existingToken) {
+      existingToken = new SpotifyToken({
+        access_token,
+        token_type,
+        expires_in,
+        refresh_token,
+        created_at: new Date(),
+      })
+    } else {
+      existingToken.access_token = access_token
+      existingToken.token_type = token
+      existingToken.expires_in = expires_in
+      existingToken.refresh_token = refresh_token
+      existingToken.created_at = new Date()
+    }
+    await existingToken.save()
+
+    // Create a JWT token
+    const jwtToken = jwt.encode(
+      { access_token, refresh_token },
+      process.env.JWT_SECRET
+    )
+
+    // Redirect to the client with the JWT token
+    res.redirect(`http://localhost:3000/?token=${jwtToken}`)
+  } catch (error) {
+    console.error(error)
+    res.send('Error occurred while getting the access token')
+  }
+})
+
+// Route to refresh the Spotify access token
+router.get('/refresh', validateJWT, async (req, res) => {
+  const { refresh_token } = req.query
+
+  try {
+    const response = await axios({
+      method: 'post',
+      url: 'https://accounts.spotify.com/api/token',
+      params: {
+        grant_type: 'refresh_token',
+        refresh_token,
+      },
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic ${Buffer.from(
+          `${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`
+        ).toString('base64')}`,
+      },
+    })
+
+    const { access_token, token_type, expires_in } = response.data
+
+    const spotifyToken = await SpotifyToken.findOne({ refresh_token })
+
+    spotifyToken.access_token = access_token
+    spotifyToken.expires_in = expires_in
+    spotifyToken.created_at = new Date()
+    await spotifyToken.save()
+
+    res.send({ access_token, expires_in })
+  } catch (error) {
+    console.error(error)
+    res
+      .status(500)
+      .send('Error occurred while refreshing the access token')
+  }
+})
 
 module.exports = router
